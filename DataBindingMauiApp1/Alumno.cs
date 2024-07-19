f61029146cdbfa70c8aaea5976571996e0687c94
angel <angel@DESKTOP-8ELEIVF>
2024-07-18 21:32:50 -0600

    Agregar archivos de proyecto.

---
+++ DataBindingMauiApp1/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataBindingMauiApp1
+{
+    public class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string nombre = string.Empty;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (nombre != value)
+                {
+                    nombre = value;
+                    OnPropertyChanged(nameof(Nombre));
+                    OnPropertyChanged(nameof(MostrarNombre));
+                }
+            }
+        }
+
+        public string MostrarNombre => $"Nombre ingresado: {nombre}";
+
+        void OnPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+}
---
+++ DataBindingMauiApp1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataBindingMauiApp1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
